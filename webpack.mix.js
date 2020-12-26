a46acca4f12c84d53e8bf875452b93be57fa3c9b
harukarist <harukarist@gmail.com>
2020-12-26 21:49:20 +0900

    first commit

---
+++ webpack.mix.js
@@ -0,0 +1,25 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix.browserSync({
+   proxy: {
+      target: "http://127.0.0.1:8000",
+   },
+   files: [
+      'resources/views/**/*.blade.php',
+   ],
+})
+   .js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
+   .version(); // バージョニングを有効にする
+mix.sourceMaps().js('node_modules/popper.js/dist/popper.js', 'public/js').sourceMaps(); // public/js内にpopper.jsを生成
