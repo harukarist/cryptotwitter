fd75034a946666e8ed3644d98c09695e876437e8
harukarist <harukarist@gmail.com>
2020-12-28 22:21:43 +0900

    APIで通貨ペア情報、ティッカー情報を取得

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,5 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/trend', 'TrendController@getTicker')->name('trend.get');
+// Route::get('/home', 'HomeController@index')->name('home');
