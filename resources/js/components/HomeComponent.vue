4b187055c988b295883288518e3a8cebdc60709d
harukarist <harukarist@gmail.com>
2020-12-31 13:01:22 +0900

    Replace template

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -1,20 +1,13 @@
 <template>
-  <div class="container">
+  <div class="container c-container">
     <div class="row justify-content-center">
-      <div class="media">
-        <ul class="list-unstyled" style="width: 500px">
-          <li class="media mb-3">
-            <img src="64x564.gif" alt="64x64" class="mr-3" />
-            <div class="media-body">
-              <h5>HTML 5.2</h5>
-              This specification defines the 5th major version, second minor
-              revision of the core language of the World Wide Web: the Hypertext
-              Markup Language (HTML). In this version, new features continue...
-            </div>
-          </li>
-          :
-        </ul>
-      </div>
+      <h5 class="c-container__title">リアルタイムトレンド</h5>
+    </div>
+    <div class="row justify-content-center">
+      <h5 class="c-container__title">関連Twitterアカウント</h5>
+    </div>
+    <div class="row justify-content-center">
+      <h5 class="c-container__title">仮想通貨 最新ニュース</h5>
     </div>
   </div>
 </template>
