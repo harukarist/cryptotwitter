c568b69265a108004710de32b884853bc2a0608c
harukarist <harukarist@gmail.com>
2021-01-27 20:59:42 +0900

    Add FLOCSS files

--- resources/js/components/HeaderComponent.vue
+++ resources/js/components/HeaderComponent.vue
@@ -1,96 +1,94 @@
 <template>
-  <div class="container-fluid bg-dark mb-3">
-    <div class="container">
-      <nav class="navbar navbar-expand-lg navbar-dark">
-        <!-- active-class="is-active"で、該当ディレクトリ内にいる時のみis-activeクラスを付与 -->
-        <!-- home '/'のみ、子のディレクトリは内包しないよう、exactオプションを使用 -->
-        <!-- サイトロゴ -->
-        <RouterLink
-          :to="{ name: 'top.index' }"
-          active-class="active"
-          exact
-          class="navbar-brand"
-        >
-          CryptoTrend
-        </RouterLink>
+  <div class="l-header is-fixed">
+    <nav class="p-navbar">
+      <!-- active-class="is-active"で、該当ディレクトリ内にいる時のみis-activeクラスを付与 -->
+      <!-- home '/'のみ、子のディレクトリは内包しないよう、exactオプションを使用 -->
+      <!-- サイトロゴ -->
+      <RouterLink
+        :to="{ name: 'top.index' }"
+        active-class="active"
+        exact
+        class="navbar-brand"
+      >
+        CryptoTrend
+      </RouterLink>
 
-        <!-- トグルアイコン -->
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+      <!-- トグルアイコン -->
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
 
-        <!-- ログイン済みユーザー向けメニュー -->
-        <div v-if="isLogin" class="collapse navbar-collapse" id="navbarNav">
-          <ul class="p-navbar navbar-nav">
-            <li class="p-navbar__item nav-item">
-              <RouterLink
-                :to="{ name: 'trend.index' }"
-                active-class="active"
-                class="nav-link"
-              >
-                トレンド一覧
-              </RouterLink>
-            </li>
-            <li class="p-navbar__item nav-item">
-              <RouterLink
-                :to="{ name: 'twitter.index' }"
-                active-class="active"
-                class="nav-link"
-              >
-                Twitterフォロー
-              </RouterLink>
-            </li>
-            <li class="p-navbar__item nav-item">
-              <RouterLink
-                :to="{ name: 'news.index' }"
-                active-class="active"
-                class="nav-link"
-              >
-                関連ニュース
-              </RouterLink>
-            </li>
-            <li class="p-navbar__item nav-item">
-              {{ username }}
-            </li>
-            <li class="p-navbar__item nav-item">
-              <a class="nav-link" @click="logout">Logout</a>
-            </li>
-          </ul>
-        </div>
+      <!-- ログイン済みユーザー向けメニュー -->
+      <div v-if="isLogin" class="collapse navbar-collapse" id="navbarNav">
+        <ul class="p-navbar navbar-nav">
+          <li class="p-navbar__item nav-item">
+            <RouterLink
+              :to="{ name: 'trend.index' }"
+              active-class="active"
+              class="nav-link"
+            >
+              トレンド一覧
+            </RouterLink>
+          </li>
+          <li class="p-navbar__item nav-item">
+            <RouterLink
+              :to="{ name: 'twitter.index' }"
+              active-class="active"
+              class="nav-link"
+            >
+              Twitterフォロー
+            </RouterLink>
+          </li>
+          <li class="p-navbar__item nav-item">
+            <RouterLink
+              :to="{ name: 'news.index' }"
+              active-class="active"
+              class="nav-link"
+            >
+              関連ニュース
+            </RouterLink>
+          </li>
+          <li class="p-navbar__item nav-item">
+            {{ username }}
+          </li>
+          <li class="p-navbar__item nav-item">
+            <a class="nav-link" @click="logout">Logout</a>
+          </li>
+        </ul>
+      </div>
 
-        <!-- 未ログインユーザー向けメニュー -->
-        <div v-if="!isLogin" class="collapse navbar-collapse" id="navbarNav">
-          <ul class="p-navbar navbar-nav">
-            <li class="p-navbar__item nav-item">
-              <RouterLink
-                :to="{ name: 'register' }"
-                active-class="active"
-                class="nav-link"
-              >
-                無料ユーザー登録
-              </RouterLink>
-            </li>
-            <li class="p-navbar__item nav-item">
-              <RouterLink
-                :to="{ name: 'login' }"
-                active-class="active"
-                class="nav-link"
-              >
-                ログイン
-              </RouterLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+      <!-- 未ログインユーザー向けメニュー -->
+      <div v-if="!isLogin" class="collapse navbar-collapse" id="navbarNav">
+        <ul class="p-navbar navbar-nav">
+          <li class="p-navbar__item nav-item">
+            <RouterLink
+              :to="{ name: 'register' }"
+              active-class="active"
+              class="nav-link"
+            >
+              無料ユーザー登録
+            </RouterLink>
+          </li>
+          <li class="p-navbar__item nav-item">
+            <RouterLink
+              :to="{ name: 'login' }"
+              active-class="active"
+              class="nav-link"
+            >
+              ログイン
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
   </div>
 </template>
 
