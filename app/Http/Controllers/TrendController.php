b655055795e235eafe84688ef0ee17aaca903678
harukarist <harukarist@gmail.com>
2021-01-30 22:33:48 +0900

    Fix json format

--- app/Http/Controllers/TrendController.php
+++ app/Http/Controllers/TrendController.php
@@ -17,20 +17,6 @@ class TrendController extends Controller
         $trends = Trend::orderBy('tweet_hour', 'DESC')->get();
         // $trends = Trend::all();
 
-        foreach ($trends as $trend) {
-            // 通貨ペアがある通貨は最高価格、最安価格の数値をカンマ区切りにフォーマット
-            if ($trend->currency_pair) {
-                $trend->high = number_format($trend->high) . ' 円';
-                $trend->low = number_format($trend->low) . ' 円';
-            } else {
-                // 通貨ペアが登録されていない通貨は'不明'を表示
-                $trend->high = '不明';
-                $trend->low = '不明';
-            }
-            $trend->tweet_hour = number_format($trend->tweet_hour);
-            $trend->tweet_day = number_format($trend->tweet_day);
-            $trend->tweet_week = number_format($trend->tweet_week);
-        }
         // バッチの最終実行日時をバッチテーブルから取得
         $batch =  Batch::select('batch_finished_at')->where('batch_name', 'update_prices')->first();
         // 日時形式を変換（Batchモデルの$datesプロパティに'batch_finished_at'カラムを指定する）
