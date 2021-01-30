66e2a446f8e549b3c1f96485e43c6a23d305f9eb
harukarist <harukarist@gmail.com>
2021-01-31 00:48:19 +0900

    Fix apiStatus

--- resources/js/store/twitter.js
+++ resources/js/store/twitter.js
@@ -5,6 +5,7 @@ import { OK } from '../utility'
 const state = {
   usersTwitter: null, //ログイン済みユーザーのTwitterアカウント情報を保持
   totalAutoFollow: 0, //自動フォローでの合計フォロー数
+  apiStatus: null, //API呼び出しの成否を格納
 }
 
 // ステートの値から状態を算出するゲッター
@@ -29,53 +30,115 @@ const mutations = {
   setUseAutoFollow(state, isActive) {
     state.usersTwitter.use_autofollow = isActive
   },
+  // apiStatusステートを更新する処理
+  setApiStatus(state, status) {
+    state.apiStatus = status
+  },
 }
 
 // 非同期処理を行い、ミューテーションにcommitするアクション
 // アクションの第一引数に、commit()などを持つコンテキストオブジェクトを渡す
 const actions = {
+
   // Twitter認証チェック
   async checkAuth(context) {
+    // setApiStatusミューテーションでステータスを初期化
+    context.commit('setApiStatus', null)
+
     // サーバーのAPIを呼び出し
-    const response = await axios.get(`api/auth/twitter/check`)
+    const response = await axios.get('api/auth/twitter/check')
 
     // API通信が成功した場合
     if (response.status === OK) {
+      // setApiStatusミューテーションでステータスをtrueに変更
+      context.commit('setApiStatus', true)
       // 返却されたユーザー情報（未ログインの場合はnull）を格納
       const usersTwitter = response.data.twitter_user || null
       const totalAutoFollow = response.data.follow_total || null
       // setUserDataミューテーションでuserDataステートを更新
       context.commit('setUsersTwitter', usersTwitter)
       context.commit('setTotalAutoFollow', totalAutoFollow)
+      return false //処理を終了
+    }
+    // API通信が失敗した場合
+    // ステータスをfalseに変更
+    context.commit('setApiStatus', false)
+    // errorモジュールのsetCodeミューテーションでステータスを更新
+    // 別モジュールのミューテーションをcommitするためroot: trueを指定する
+    context.commit('error/setCode', response.status, { root: true })
+  },
+
+  // Twitterアカウント連携を削除
+  async deleteAuth(context) {
+    // setApiStatusミューテーションでステータスを初期化
+    context.commit('setApiStatus', null)
+
+    // サーバーのAPIを呼び出し
+    const response = await axios.get('api/auth/twitter/delete')
+
+    // API通信が成功した場合
+    if (response.status === OK) {
+      // setApiStatusミューテーションでステータスをtrueに変更
+      context.commit('setApiStatus', true)
+      // ユーザー情報のステートをnullに初期化
+      context.commit('setUsersTwitter', null)
+      context.commit('setTotalAutoFollow', null)
       return false
     }
+    // API通信が失敗した場合
+    // ステータスをfalseに変更
+    context.commit('setApiStatus', false)
+    // errorモジュールのsetCodeミューテーションでステータスを更新
+    // 別モジュールのミューテーションをcommitするためroot: trueを指定する
+    context.commit('error/setCode', response.status, { root: true })
   },
+
   // 自動フォロー利用開始
   async applyAutoFollow(context) {
+    // setApiStatusミューテーションでステータスを初期化
+    context.commit('setApiStatus', null)
+
     // サーバーのAPIを呼び出し
     const response = await axios.get('api/autofollow/apply')
-    // API通信が失敗した場合
-    if (response.status !== OK) {
-      this.$store.commit('error/setCode', response.status, { root: true })
+
+    // API通信が成功した場合
+    if (response.status === OK) {
+      // setApiStatusミューテーションでステータスをtrueに変更
+      context.commit('setApiStatus', true)
+      // 自動フォロー利用フラグをtrueに更新
+      context.commit('setUseAutoFollow', true)
       return false
     }
-    // API通信が成功した場合は自動フォロー利用フラグをtrueに更新
-    // 自動フォロー利用フラグが切り替わるためミューテーションでuserDataステートを更新する
-    context.commit('setUseAutoFollow', true)
-    return false
+    // API通信が失敗した場合
+    // ステータスをfalseに変更
+    context.commit('setApiStatus', false)
+    // errorモジュールのsetCodeミューテーションでステータスを更新
+    // 別モジュールのミューテーションをcommitするためroot: trueを指定する
+    context.commit('error/setCode', response.status, { root: true })
   },
+
   // 自動フォロー利用解除
   async cancelAutoFollow(context) {
+    // setApiStatusミューテーションでステータスを初期化
+    context.commit('setApiStatus', null)
+
     // サーバーのAPIを呼び出し
     const response = await axios.get('api/autofollow/cancel')
-    // API通信が失敗した場合
-    if (response.status !== OK) {
-      this.$store.commit('error/setCode', response.status, { root: true })
+
+    // API通信が成功した場合
+    if (response.status === OK) {
+      // setApiStatusミューテーションでステータスをtrueに変更
+      context.commit('setApiStatus', true)
+      // 自動フォロー利用フラグをfalseに更新
+      context.commit('setUseAutoFollow', false)
       return false
     }
-    // API通信が成功した場合は自動フォロー利用フラグをfalseに更新
-    context.commit('setUseAutoFollow', false)
-    return false
+    // API通信が失敗した場合
+    // ステータスをfalseに変更
+    context.commit('setApiStatus', false)
+    // errorモジュールのsetCodeミューテーションでステータスを更新
+    // 別モジュールのミューテーションをcommitするためroot: trueを指定する
+    context.commit('error/setCode', response.status, { root: true })
   },
 }
 
