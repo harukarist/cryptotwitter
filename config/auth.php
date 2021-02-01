e9dfc0aca7169e98fd0d32dfd3cf131481c9ce22
harukarist <harukarist@gmail.com>
2021-02-02 05:55:37 +0900

    Refactor comment

--- config/auth.php
+++ config/auth.php
@@ -96,7 +96,7 @@ return [
         'users' => [
             'provider' => 'users',
             'table' => 'password_resets',
-            'expire' => 60,
+            'expire' => 60, //パスワードリセットトークンの有効時間（分）
             'throttle' => 60,
         ],
     ],
