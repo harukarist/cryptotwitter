65f6a330176fe35301630d1a39d97aa19fcc6e06
harukarist <harukarist@gmail.com>
2021-02-08 22:15:40 +0900

    Fix Autofollow

--- app/Http/Controllers/AutoFollowController.php
+++ app/Http/Controllers/AutoFollowController.php
@@ -37,9 +37,6 @@ class AutoFollowController extends Controller
     // 自動フォローを解除
     public function cancelAutoFollow()
     {
-        // ログインユーザーの自動フォロー利用フラグをfalseに更新
-        $user_id = Auth::id();
-
         // ログインユーザーのIDを取得
         $user_id = Auth::id();
         // ログインユーザーのレコードをユーザーIDで検索して取得
@@ -57,7 +54,29 @@ class AutoFollowController extends Controller
     }
 
     /**
-     * 自動フォロー
+     * ログインユーザーの自動フォロー累計数を集計して返却
+     */
+    public function countAutoFollow()
+    {
+        // // ログインユーザーのIDを取得
+        // $user_id = Auth::id();
+        // // ログインユーザーのレコードをユーザーIDで検索して取得
+        // $twitter_user = TwitterUser::where('user_id', $user_id)->first();
+        // ログインユーザーのTwitterアカウント情報を取得
+        $twitter_user = Auth::user()->twitter_user()->first();
+        // Twitterアカウントの登録がある場合、自動フォローログDBからログインユーザーの自動フォロー数の合計を取得
+        if ($twitter_user) {
+            $follow_total = DB::table('autofollow_logs')->where('twitter_user_id', $twitter_user->id)
+                ->sum('follow_total');
+        } else {
+            $follow_total = 0;
+        }
+        return $follow_total;
+    }
+
+
+    /**
+     * 自動フォローを行うバッチ処理
      */
     public function autoFollow()
     {
@@ -66,7 +85,7 @@ class AutoFollowController extends Controller
 
         // ターゲット一覧（フォロー対象のTwitterアカウント一覧）を取得
         $target_users = TargetUser::select('id', 'twitter_id')->get();
-        
+
         $max_requests = 15; //自動フォローするアカウント数の上限（15分に15回まで）
         $today = Carbon::today();
 
