72ba310801df6bdc28b80db9c290ff3122117c44
harukarist <harukarist@gmail.com>
2021-02-14 20:51:58 +0900

    Add search component

---
+++ resources/js/components/SearchFormComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <form class="c-search__form u-mb--l" @submit.prevent="search">
+    <span class="c-search__icon"><i class="fas fa-search"></i></span>
+    <input
+      type="search"
+      class="c-input c-search__input"
+      name="search"
+      v-model="searchWord"
+      placeholder="ニュースをキーワード検索"
+      @keydown.enter.prevent="search"
+    />
+    <span class="c-search__clear-icon">
+      <i v-show="this.searchWord" class="fas fa-times" @click="clearWord"></i>
+    </span>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchWord: "", // 検索キーワード（v-modelでフォームの入力値と紐付け）
+    };
+  },
+  methods: {
+    search() {
+      this.$emit("search", this.searchWord);
+    },
+    clearWord() {
+      this.searchWord = "";
+      this.$emit("clear");
+    },
+  },
+};
+</script>
--- resources/js/pages/NewsListComponent.vue
+++ resources/js/pages/NewsListComponent.vue
@@ -3,40 +3,46 @@
     <section class="c-section">
       <h5 class="c-section__title">関連ニュース一覧</h5>
       <p class="c-section__text">
-        仮想通貨に関する最新ニュースを<br class="u-sp--only">お届けします。<br />
+        仮想通貨に関する最新ニュースを<br
+          class="u-sp--only"
+        />お届けします。<br />
       </p>
-      <div class="p-news">
-        <div class="p-news__item" v-for="item in news" :key="item.title">
-          <div class="p-news__info">
-            <span class="p-news__date">
-              {{ item.published_date }}
-            </span>
-            <span class="p-news__source">
-              {{ item.source }}
-            </span>
-          </div>
-          <h5 class="p-news__title">
-            <a :href="item.url" target="_blank">
-              {{ item.title }}
-            </a>
-          </h5>
-        </div>
-      </div>
+      <div class="p-news c-fade--in">
+        <SearchFormComponent @search="searchNews" @clear="clearResult" />
 
-      <div class="c-pagination">
-        <Pagination
-          :directory="directoryName"
-          :current-page="currentPage"
-          :last-page="lastPage"
-          :per-page="perPage"
-          :total-items="totalItems"
-        />
-        <PaginationInfo
-          :current-page="currentPage"
-          :per-page="perPage"
-          :total-items="totalItems"
-          :items-length="news.length"
-        />
+        <transition-group tag="div" name="popup" class="p-news__item">
+          <div class="p-news__item" v-for="item in news" :key="item.title">
+            <div class="p-news__info">
+              <span class="p-news__date">
+                {{ item.published_date }}
+              </span>
+              <span class="p-news__source">
+                {{ item.source }}
+              </span>
+            </div>
+            <h5 class="p-news__title">
+              <a :href="item.url" target="_blank">
+                {{ item.title }}
+              </a>
+            </h5>
+          </div>
+        </transition-group>
+
+        <div class="c-pagination">
+          <Pagination
+            :directory="directoryName"
+            :current-page="currentPage"
+            :last-page="lastPage"
+            :per-page="perPage"
+            :total-items="totalItems"
+          />
+          <PaginationInfo
+            :current-page="currentPage"
+            :per-page="perPage"
+            :total-items="totalItems"
+            :items-length="news.length"
+          />
+        </div>
       </div>
     </section>
   </div>
@@ -46,11 +52,13 @@
 import { OK } from "../utility";
 import Pagination from "../components/Pagination.vue";
 import PaginationInfo from "../components/PaginationInfo.vue";
+import SearchFormComponent from "../components/SearchFormComponent.vue";
 
 export default {
   components: {
-    Pagination,
-    PaginationInfo,
+    Pagination, //ページ番号付きページネーション（PC、タブレットで表示）
+    PaginationInfo, //ページネーションの件数表示
+    SearchFormComponent,
   },
   data() {
     return {
@@ -71,9 +79,21 @@ export default {
     },
   },
   methods: {
+    searchNews(word) {
+      let params = {
+        params: {
+          search: word,
+        },
+      };
+      this.fetchNews(params);
+    },
+    clearResult() {
+      let params = {};
+      this.fetchNews(params);
+    },
     // axiosでニュース一覧取得APIにリクエスト
-    async fetchNews() {
-      const response = await axios.get(`/api/news?page=${this.page}`);
+    async fetchNews(params) {
+      const response = await axios.get(`/api/news?page=${this.page}`, params);
       if (response.status !== OK) {
         // 通信失敗の場合
         this.$store.commit("error/setCode", response.status);
