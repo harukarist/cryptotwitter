69e63f7dc1b365b641ca2384ac2e2f9937b26442
harukarist <harukarist@gmail.com>
2021-02-14 12:18:15 +0900

    Refactor comments

--- app/Http/Requests/ChangePasswordRequest.php
+++ app/Http/Requests/ChangePasswordRequest.php
@@ -11,17 +11,17 @@ class ChangePasswordRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
-     *
+     * ユーザーがこの要求を行うことを許可するかどうか
      * @return bool
      */
     public function authorize()
     {
-        return true;
+        return true; //要求を許可
     }
 
     /**
      * Get the validation rules that apply to the request.
-     *
+     * リクエストに適用するバリデーションルールを指定
      * @return array
      */
     public function rules()
