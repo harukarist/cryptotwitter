2190ab96a48e43cca257fa1cbb2d09c91ecb1156
harukarist <harukarist@gmail.com>
2021-02-16 20:12:26 +0900

    Fix updated_at

--- app/Http/Controllers/TrendController.php
+++ app/Http/Controllers/TrendController.php
@@ -15,19 +15,20 @@ class TrendController extends Controller
      */
     public function index()
     {
-        // トレンドテーブルから全データを取得し、ツイート数の多い順にレコードを並べ替える
-        $trends = Trend::orderBy('tweet_hour', 'DESC')->get();
+        // trendsテーブルから全データを取得し、ツイート数の多い順にレコードを並べ替える
+        $trends = Trend::orderBy('tweet_hour', 'desc')->get();
 
-        // バッチの最終実行日時をバッチテーブルから取得
-        $batch =  Batch::select('batch_finished_at')->where('batch_name', 'update_prices')->first();
+        // trendsテーブルの全レコードから最新の更新日時を取得
+        $trend_updated =  Trend::select('updated_at')
+            ->orderBy('updated_at', 'desc')->first();
 
         // 取得できなかった場合は NotFoundエラーを返却
-        if (!$trends || !$batch) {
+        if (!$trends || !$trend_updated) {
             return abort(404);
         }
 
         // 日時形式を変換（Batchモデルの$datesプロパティに'batch_finished_at'カラムを指定する）
-        $updated_at = $batch->batch_finished_at->format('Y.m.d H:i');
+        $updated_at = $trend_updated->updated_at->format('Y.m.d H:i');
         // 各通貨の情報と更新日時を配列に格納
         $items['trends'] = $trends;
         $items['updated_at'] = $updated_at;
